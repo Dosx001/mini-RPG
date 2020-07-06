66ec8628678481806ea214637f743e6844cdcddf
Andres Rodriguez <andresmichelrodriguez@gmail.com>
2020-07-05 23:08:12 -0700

    Created a struct of pointers for each Player stat to transfer info into Classes

--- Classes.h
+++ Classes.h
@@ -1,7 +1,20 @@
+#pragma once
 #include "pch.h"
+
+struct Ptrs {
+    public:
+        int *ptrSpd;
+        int *ptrLuk;
+        int *ptrInt;
+        int *ptrWid;
+        int *ptrStr;
+        int *ptrChr;
+};
 
 class Class {
     public:
+        Ptrs stats;
+//        Class(Ptrs ptrsStats); 
         virtual void test(); 
         virtual int attack();
         virtual int defense();
