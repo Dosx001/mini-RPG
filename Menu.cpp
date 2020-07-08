fb4144b3e9816196c97c2a3d23b4b7f718db0d82
Andres Rodriguez <andresmichelrodriguez@gmail.com>
2020-07-08 16:30:11 -0700

    Reformatting and added Inventory getNumItems() to FullMenu()

--- Menu.cpp
+++ Menu.cpp
@@ -1,26 +1,28 @@
 #include "Menu.h"
 
-Menu::Menu(Player *Py, Inventory *Invy) :ptrPy(Py), ptrInvy(Invy) {}
+Menu::Menu(Player* Py, Inventory* Invy) :ptrPy(Py), ptrInvy(Invy) {}
 
 void Menu::quickMenu() {
     std::cout << "Full Menu | Inventory | Quit" << std::endl;
 }
 
 void Menu::fullMenu() {
-    std::cout << "Name: " << ptrPy -> getName() << " | Class: " <<
-    ptrPy -> getClassType() << std::endl;
-    std::cout << "Health: " << ptrPy -> getHealth() << 
-    " | Mana: " << ptrPy -> getMana() << std::endl; 
+    std::cout << "Name: " << ptrPy -> getName() << " | Class: "
+        << ptrPy -> getClassType() << std::endl;
+    std::cout << "Health: " << ptrPy -> getHealth()
+        << " | Mana: " << ptrPy -> getMana() << std::endl; 
     std::cout << "Stats: " << "Luck=" << ptrPy -> getLuck() << " Speed="
-    << ptrPy -> getSpeed() << " Intel=" << ptrPy -> getIntel() << " Wisdom="
-    << ptrPy -> getWisdom() << " Strength=" << ptrPy -> getStrength()
-    << " Charisma=" << ptrPy -> getCharisma() << std::endl;
+        << ptrPy -> getSpeed() << " Intel=" << ptrPy -> getIntel() << " Wisdom="
+        << ptrPy -> getWisdom() << " Strength=" << ptrPy -> getStrength()
+        << " Charisma=" << ptrPy -> getCharisma() << std::endl;
     if (ptrInvy -> getNumItems() == 0) {
         std::cout << "Inventory: empty" << std::endl;
     } else {
-        std::cout << "Inventory: Capacity=" << ptrInvy -> getSize() << std::endl;
+        std::cout << "Inventory: " << ptrInvy -> getNumItems()
+           << "/" <<  ptrInvy -> getSize() << std::endl;
         ptrInvy -> displayItems();
     }
-    std::cout << "Health Potions: " << ptrInvy -> getHP() << "/" << ptrInvy -> getMaxPotions() <<
-    " Mana Potions: " << ptrInvy -> getMP() << "/" << ptrInvy -> getMaxPotions() << std::endl;
+    std::cout << "Health Potions: " << ptrInvy -> getHP() << "/"
+        << ptrInvy -> getMaxPotions() << " Mana Potions: "
+        << ptrInvy -> getMP() << "/" << ptrInvy -> getMaxPotions() << std::endl;
 }
