cb5998f3588774bf044c77dd1f5f5eb0a5e58b15
Andres Rodriguez <andresmichelrodriguez@gmail.com>
2020-07-09 16:47:39 -0700

    Created wisdom() and findMatch()

--- Trials.h
+++ Trials.h
@@ -5,7 +5,10 @@ class Trials{
     public:        
         unsigned short speed();
         unsigned short luck(); 
-        unsigned short  intel(); 
+        unsigned short intel(); 
+        unsigned short wisdom();
+    private:
+        int findMatch(std::string str, std::string reg);
 };
 
 void sleep(short time);
