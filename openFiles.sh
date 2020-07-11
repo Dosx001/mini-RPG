486264043f1144b4c1d3cd5c98503c69f97632bf
Andres Rodriguez <andresmichelrodriguez@gmail.com>
2020-07-10 17:07:06 -0700

    New file

---
+++ openFiles.sh
@@ -0,0 +1,8 @@
+vim Player.h Inventory.h  Trials.h Menu.h Classes.h pch.h Makefile -p \
+    "+vs Player.cpp | tabn" \
+    "+vs Inventory.cpp | tabn" \
+    "+vs Trials.cpp | tabn" \
+    "+vs Menu.cpp | tabn" \
+    "+vs Classes.cpp | tabn" \
+    "+vs pch.cpp | tabn" \
+    "+vs main.cpp | tabmove 0"
