d19a87ca288174acf6a241a7fc9b395a174f78f1
Andres Rodriguez <andresmichelrodriguez@gmail.com>
2020-07-11 20:33:39 -0700

    Edited to reflect the new directory structure

--- openFiles.sh
+++ openFiles.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-vim Player.h Inventory.h Trials.h Menu.h Classes.h pch.h Makefile -p \
-    "+vs Player.cpp | tabn" \
-    "+vs Inventory.cpp | tabn" \
-    "+vs Trials.cpp | tabn" \
-    "+vs Menu.cpp | tabn" \
-    "+vs Classes.cpp | tabn" \
-    "+vs pch.cpp | tabn" \
-    "+vs main.cpp | tabmove 0"
+cd include/
+vim Player.hpp Inventory.hpp Trials.hpp Menu.hpp Classes.hpp pch.hpp ../Makefile -p \
+    "+vs ../src/Player.cpp | tabn" \
+    "+vs ../src/Inventory.cpp | tabn" \
+    "+vs ../src/Trials.cpp | tabn" \
+    "+vs ../src/Menu.cpp | tabn" \
+    "+vs ../src/Classes.cpp | tabn" \
+    "+vs ../src/pch.cpp | tabn" \
+    "+vs ../src/main.cpp | tabmove 0"
