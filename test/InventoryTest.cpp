5b59313c6400c8b769a3d841e70d4adcf425887f
Andres Rodriguez <andresmichelrodriguez@gmail.com>
2020-07-15 17:36:02 -0700

    Created a fixture

--- test/InventoryTest.cpp
+++ test/InventoryTest.cpp
@@ -1,27 +1,36 @@
 #include <gtest/gtest.h>
 #include "Inventory.hpp"
 
-TEST(getNumItems_Test, AddRemoveItems) {
-    Inventory Invy;
-    ASSERT_EQ(Invy.getSize(), 10);
-    ASSERT_EQ(Invy.getNumItems(), 0);
+class Fixture: public testing::Test {
+    protected:
+        Inventory* Invy;
+        void SetUp() {
+            Invy = new Inventory;
+        }
+        void TearDown() {
+            delete Invy;
+        }
+};
+
+TEST_F(Fixture, getNumItems_Test) {
+    ASSERT_EQ(Invy -> getSize(), 10);
+    ASSERT_EQ(Invy -> getNumItems(), 0);
     for (int i = 0; i < 5; i++) {
-        Invy.addItem("item");
+        Invy -> addItem("item");
     }
-    ASSERT_EQ(Invy.getNumItems(), 5);
-    Invy.removeItem("item");
-    ASSERT_EQ(Invy.getNumItems(), 5); //FIX BUG
+    ASSERT_EQ(Invy -> getNumItems(), 5);
+    Invy -> removeItem("item");
+    ASSERT_EQ(Invy -> getNumItems(), 5); //FIX BUG
 }
 
-TEST(getHP_getMP_Test, AddRemovePotions) {
-    Inventory Invy;
-    ASSERT_EQ(Invy.getHP(), 2);
-    Invy.addHP();
-    ASSERT_EQ(Invy.getHP(), 3);
+TEST_F(Fixture, getHP_getMP_Test) {
+    ASSERT_EQ(Invy -> getHP(), 2);
+    Invy -> addHP();
+    ASSERT_EQ(Invy -> getHP(), 3);
     for (int i = 0; i < 5; i++) {
-        Invy.addMP();
+        Invy -> addMP();
     }
-    ASSERT_EQ(Invy.getMP(), 7);
+    ASSERT_EQ(Invy -> getMP(), 7);
 }
 
 int main(int argc, char **argv) {
