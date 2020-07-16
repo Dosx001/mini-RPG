3834cd8203f2723147ed8b816e9d0f195f866b6f
Andres Rodriguez <andresmichelrodriguez@gmail.com>
2020-07-16 10:25:52 -0700

    Remove Makefile

--- scripts/openFiles.sh
+++ scripts/openFiles.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-vim include/Player.hpp include/Inventory.hpp include/Trials.hpp include/Menu.hpp \
-    include/Classes.hpp include/pch.hpp Makefile -p \
+vim -p include/Player.hpp include/Inventory.hpp include/Trials.hpp \
+    include/Menu.hpp include/Classes.hpp include/pch.hpp src/main.cpp \
     "+vs src/Player.cpp | tabn" \
     "+vs src/Inventory.cpp | tabn" \
     "+vs src/Trials.cpp | tabn" \
     "+vs src/Menu.cpp | tabn" \
     "+vs src/Classes.cpp | tabn" \
     "+vs src/pch.cpp | tabn" \
-    "+vs src/main.cpp | tabmove 0"
+    "+tabmove 0"
