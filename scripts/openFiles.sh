b8509c1ce0c1fa8d4b0539900c40d5795ba3fffa
Andres Rodriguez <andresmichelrodriguez@gmail.com>
2020-07-17 20:17:41 -0700

    Added Enemy and Battle class files

--- scripts/openFiles.sh
+++ scripts/openFiles.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 vim -p include/Player.hpp include/Inventory.hpp include/Trials.hpp \
-    include/Menu.hpp include/Classes.hpp include/pch.hpp src/main.cpp \
+    include/Menu.hpp include/Classes.hpp include/Enemy.hpp include/Battle.hpp \
+    include/pch.hpp src/main.cpp \
     "+vs src/Player.cpp | tabn" \
     "+vs src/Inventory.cpp | tabn" \
     "+vs src/Trials.cpp | tabn" \
     "+vs src/Menu.cpp | tabn" \
     "+vs src/Classes.cpp | tabn" \
+    "+vs src/Enemy.cpp | tabn" \
+    "+vs src/Battle.cpp | tabn" \
     "+vs src/pch.cpp | tabn" \
     "+tabmove 0"
