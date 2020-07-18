b1c22e91bd8ec47910c5f55d284558fbfc48cc03
Andres Rodriguez <andresmichelrodriguez@gmail.com>
2020-07-18 11:58:58 -0700

    Edited for new cmake structure

--- scripts/coverage.sh
+++ scripts/coverage.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 root=`pwd`
-rm -rf coverage/$1Coverage
-rm include/CMakeFiles/lib.dir/__/src/*.gcda
-make
+rm -rf coverage
+rm CMakeFiles/lib.dir/src/$1.gcda
+make $1Test.exe
 ./bin/$1Test.exe
-cd include/CMakeFiles/lib.dir/__/src
+cd CMakeFiles/lib.dir/src
 gcov $1.gcno
 lcov --capture --directory . --output-file $1.info
-genhtml $1.info --output-directory $1Coverage
-mv $1Coverage ${root}/coverage
-cd ${root}
-./bin/$1Test.exe
+genhtml $1.info --output-directory coverage
+mv coverage ${root}
