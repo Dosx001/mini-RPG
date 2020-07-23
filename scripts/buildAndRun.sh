97ac5ef9bdd3980b584107575e22039122d5d71e
Andres Rodriguez <andresmichelrodriguez@gmail.com>
2020-07-23 10:52:24 -0700

    Added Warrior and Sorcerer support

--- scripts/buildAndRun.sh
+++ scripts/buildAndRun.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 make mini_RPG.exe
-expect scripts/run.exp
+if [ $# == 0 ]
+then
+    expect scripts/run.exp
+elif [ $1 == 'W' ]
+then
+    expect scripts/run.exp W
+elif [ $1 == 'S' ]
+then
+    expect scripts/run.exp S
+fi
