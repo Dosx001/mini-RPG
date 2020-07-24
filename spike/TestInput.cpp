a877e29c8bf8357e923b1e8e00c3cfb8215e5c5b
Andres Rodriguez <andresmichelrodriguez@gmail.com>
2020-07-23 17:08:06 -0700

    New test file

---
+++ spike/TestInput.cpp
@@ -0,0 +1,31 @@
+#include <gtest/gtest.h>
+#include <iostream>
+#include <fstream>
+
+int readUserInput(std::istream& input)
+{
+    int value;
+    std::cout << "Enter a number: ";
+    input >> value;
+    return value;
+}
+
+TEST(Some, Test) {
+    std::ifstream ifs;
+    ifs.open("spike/input.txt", std::ifstream::in);
+    //std::cout << "readUserInput from std::cin: " << readUserInput(std::cin) << std::endl;
+    std::cout << "readUserInput from ifs: " << readUserInput(ifs) << std::endl;
+    std::cout << "readUserInput from ifs: " << readUserInput(ifs) << std::endl;
+    ifs.close();
+    testing::internal::CaptureStdout();
+    std::cout << "test\n";
+    std::string output = testing::internal::GetCapturedStdout();
+    if (output == "test\n") {
+        std::cout << "work\n";
+    }
+}
+
+int main(int argc, char **argv) {
+    testing::InitGoogleTest();
+    return RUN_ALL_TESTS();
+}
