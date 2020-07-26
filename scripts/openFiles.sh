3ef6e917f505112e1bd52ca2ebf94d01e443be7c
Andres Rodriguez <andresmichelrodriguez@gmail.com>
2020-07-25 19:41:24 -0700

    Added more setups with different files.

--- scripts/openFiles.sh
+++ scripts/openFiles.sh
@@ -1,13 +1,28 @@
 #!/bin/bash
-vim -p include/Player.hpp include/Inventory.hpp include/Trials.hpp \
-    include/Menu.hpp include/Classes.hpp include/Enemy.hpp include/Battle.hpp \
-    include/pch.hpp src/main.cpp \
-    "+vs src/Player.cpp | tabn" \
-    "+vs src/Inventory.cpp | tabn" \
-    "+vs src/Trials.cpp | tabn" \
-    "+vs src/Menu.cpp | tabn" \
-    "+vs src/Classes.cpp | tabn" \
-    "+vs src/Enemy.cpp | tabn" \
-    "+vs src/Battle.cpp | tabn" \
-    "+vs src/pch.cpp | tabn" \
-    "+tabmove 0"
+if [ $# == 0 ]
+then
+    vim -p include/Player.hpp include/Inventory.hpp include/Trials.hpp \
+        include/Menu.hpp include/Classes.hpp include/Enemy.hpp include/Battle.hpp \
+        include/pch.hpp src/main.cpp \
+        "+vs src/Player.cpp | tabn" \
+        "+vs src/Inventory.cpp | tabn" \
+        "+vs src/Trials.cpp | tabn" \
+        "+vs src/Menu.cpp | tabn" \
+        "+vs src/Classes.cpp | tabn" \
+        "+vs src/Enemy.cpp | tabn" \
+        "+vs src/Battle.cpp | tabn" \
+        "+vs src/pch.cpp | tabn" \
+        "+tabmove 0"
+elif [ $1 == 's' ]
+then
+    vim -p scripts/run.exp scripts/coverage.sh scripts/openFiles.sh \
+        "+vs scripts/buildAndRun.sh"
+elif [ $1 == 'c' ]
+then
+    vim -O CMakeLists.txt spike/CMakeLists.txt
+elif [ $1 == 't' ]
+then
+    vim -p src/Enemy.cpp src/Inventory.cpp \
+        "+vs test/EnemyTest.cpp | tabn" \
+        "+vs test/InventoryTest.cpp | tabmove 0"
+fi
