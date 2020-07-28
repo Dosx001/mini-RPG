11b815fbb5093d8db4bf5ae895c6ca0ec4b5c817
Andres Rodriguez <andresmichelrodriguez@gmail.com>
2020-07-27 19:32:55 -0700

    Added Maze class

--- src/main.cpp
+++ src/main.cpp
@@ -3,15 +3,19 @@
 #include "Menu.hpp"
 #include "Enemy.hpp"
 #include "Battle.hpp"
+#include "Maze.hpp"
 
 int main() {
+    system("clear");
     Inventory Invy;
     Player Py(Invy);
     Menu Mu(Py, Invy);
+    Maze Ma;
     std::cin.ignore();
     std::string line(50, '_');
     std::string input = "Start";
     while (input != "Quit" and Py.getHealth() != 0) {
+        system("clear");
         Mu.quickMenu();
         getline(std::cin, input);
         std::cout << line << '\n';
@@ -22,6 +26,7 @@ int main() {
             Invy.displayItems();
         }
         std::cout << '\n';
+        Ma.move();
         Enemy Ey;
         Battle Ba(Py, Ey, Mu);
     }
